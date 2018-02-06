84910e38e94261c6a86198f34befa154ecb971aa
vbaghdas <vbaghdas@gmail.com>
2018-02-06 00:31:40 -0800

    added subscribe form

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -3,29 +3,50 @@
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
 
+// Create an instance of php mailer
+$mail = new PHPMailer;
+
+// Set a host
+$mail->Host = 'smtp.gmail.com';
+
+// Set mailer to use SMTP.
+$mail->isSMTP();
+
+// Enable SMTP authentication
+$mail->SMTPAuth = true;         
+
+// Contact Form
 $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
 $visitor_email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
 $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
 $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-$mail = new PHPMailer;
-$mail->Host = 'smtp.gmail.com';
-$mail->isSMTP();
-$mail->SMTPAuth = true;         
+// Set login details for gmail account
 $mail->Username = EMAIL_USER;
 $mail->Password = EMAIL_PASS;
+
+// Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
 $mail->SMTPSecure = 'tls';    
+
+// TCP port to connect to
 $mail->Port = 587;
+
 $mail->smtpConnect($options);
-$mail->isHTML(true);
+// Sender's email address (shows in "From" field)
+$mail->From = $visitor_email;
 
-
-$mail->From = $visitor_email; // Sender's email address (shows in "From" field)
-$mail->FromName = $name; // Sender's name (shows in "From" field)
+// Sender's name (shows in "From" field)
+$mail->FromName = $name;
 
 // Add a recipient
 $mail->addAddress('outlawstatus2@gmail.com', 'First Recipient');
+
+// Add a reply-to address
 $mail->addReplyTo($visitor_email);                          
+
+// Set email format to HTML
+$mail->isHTML(true);
+
 $mail->Subject = $subject;
 $mail->Body    = $message;
 
@@ -35,22 +56,6 @@ if(!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
     echo 'Message has been sent';
-    $automail = new PHPMailer(true);
-    $automail->IsSMTP();               // set mailer to use SMTP
-    $automail->Host = "smtp.gmail.com";  // specify main and backup server or localhost
-    $automail->SMTPAuth = true;     // turn on SMTP authentication
-    $automail->Username = EMAIL_USER;  // SMTP username
-    $automail->Password = EMAIL_PASS; // SMTP password
-    $automail->SMTPSecure = 'tls';    
-    $automail->Port = 587;
-    $automail->smtpConnect($options);
-    $automail->isHTML(true);
-    $automail->From = "bimchirola@gmail.com";
-    $automail->FromName = "Back In Motion | Health Center";
-    $automail->AddAddress(visitor_email, $name);
-    $automail->Subject = "Autoresponse: We received your submission";
-    $automail->Body = "We received your submission. We will contact you soon ...";
-    $autoemail->send();
 }
 
 ?>
